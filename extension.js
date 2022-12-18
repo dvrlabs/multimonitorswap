d742fdd8d37d459eb039ab81c97a0e3c9e07cd91
DVR <dvrlabs.tv@gmail.com>
2022-12-17 19:58:51 -0500

    Simplified code

--- extension.js
+++ extension.js
@@ -12,233 +12,60 @@ const SCHEMA_SWAP_LEFT = 'swap-left';
 
 class MultiMonitorSwap {
     constructor() {
-        this._workspaceManager = global.workspace_manager;
         this._keySwapUpId = null;
         this._keySwapDownId = null;
         this._keySwapRightId = null;
         this._keySwapLeftId = null;
-        this._activeWindow = null;
     }
 
-    swapWindow(id) {
-        const { activeWindow, activeRect } = this._getActiveWindow();
-        if (!activeWindow) {
-            this._activeWindow = null;
-            return;
-        }
+    swapWindow(direction) {
+        // Acquire the active focused window as a starting point.
+        const activeWindow = this._getActiveWindow();
+        if (!activeWindow) return;
 
-        this._activeWindow = activeWindow;
-        const bestCandidate = this._getBestCandidate(id, activeWindow.get_monitor(), activeRect);
+        // Reassign the directional constants to the required enum for acquring neighbor index.
+        if (direction == 'swap-up') direction = Meta.DisplayDirection.UP;
+        if (direction == 'swap-down') direction = Meta.DisplayDirection.DOWN;
+        if (direction == 'swap-right') direction = Meta.DisplayDirection.RIGHT;
+        if (direction == 'swap-left') direction = Meta.DisplayDirection.LEFT;
 
+        // Accordings to active window display & monitor,
+        // find the next monitor by given direction. 
+        // find the inactive window, on the next monitor
+        let currentMonitor = activeWindow.get_monitor();
+        let nextMonitor = global.display.get_monitor_neighbor_index(currentMonitor, direction);
+        const windows = this._getAllWindows(nextMonitor);
+        const inertWindow = windows[0]; 
 
-        if (bestCandidate) {
-            let currentMonitorIndex = this._activeWindow.get_monitor();
-            let nextMonitorIndex = bestCandidate.get_monitor();
+        //Swap the windows to each others monitors. 
+        inertWindow.move_to_monitor(currentMonitor); 
+        activeWindow.move_to_monitor(nextMonitor);
 
-            bestCandidate.move_to_monitor(currentMonitorIndex);
-            this._activeWindow.move_to_monitor(nextMonitorIndex) 
+        //Keep focus on the original monitor...
+        activeWindow.activate(global.get_current_time());
 
-            //bestCandidate.activate(global.get_current_time());
-            this._activeWindow.activate(global.get_current_time());
-        }
-    }
-
-    _getMonitorByDirection(id, activeMonitorId) {
-        const numberOfMonitors = this._activeWindow.get_display().get_n_monitors();
-
-        let activeMonitor = null;
-        const monitors = [];
-        for (let i = 0; i < numberOfMonitors; i++) {
-            if (i === activeMonitorId)
-                activeMonitor = this._activeWindow.get_display().get_monitor_geometry(i);
-            else
-                monitors.push({ id: i, rect: this._activeWindow.get_display().get_monitor_geometry(i) });
-        }
-
-        if (!activeMonitor || !monitors.length)
-            return null;
-
-        let bestMonitorCandidate = null;
-        switch (id) {
-        case SCHEMA_SWAP_UP:
-            for (let m of monitors) {
-                if (m.rect.y < activeMonitor.y) {
-                    if (!bestMonitorCandidate)
-                        bestMonitorCandidate = m;
-                    else if (
-                        Math.abs(activeMonitor.x - m.rect.x) <
-              Math.abs(activeMonitor.x - bestMonitorCandidate.rect.x)
-                    )
-                        bestMonitorCandidate = m;
-                }
-            }
-            break;
-        case SCHEMA_SWAP_DOWN:
-            for (let m of monitors) {
-                if (m.rect.y > activeMonitor.y) {
-                    if (!bestMonitorCandidate)
-                        bestMonitorCandidate = m;
-                    else if (
-                        Math.abs(activeMonitor.x - m.rect.x) <
-              Math.abs(activeMonitor.x - bestMonitorCandidate.rect.x)
-                    )
-                        bestMonitorCandidate = m;
-                }
-            }
-            break;
-        case SCHEMA_SWAP_RIGHT:
-            for (let m of monitors) {
-                if (m.rect.x > activeMonitor.x) {
-                    if (!bestMonitorCandidate)
-                        bestMonitorCandidate = m;
-                    else if (
-                        Math.abs(activeMonitor.y - m.rect.y) <
-              Math.abs(activeMonitor.y - bestMonitorCandidate.rect.y)
-                    )
-                        bestMonitorCandidate = m;
-                }
-            }
-            break;
-        case SCHEMA_SWAP_LEFT:
-            for (let m of monitors) {
-                if (m.rect.x < activeMonitor.x) {
-                    if (!bestMonitorCandidate)
-                        bestMonitorCandidate = m;
-                    else if (
-                        Math.abs(activeMonitor.y - m.rect.y) <
-              Math.abs(activeMonitor.y - bestMonitorCandidate.rect.y)
-                    )
-                        bestMonitorCandidate = m;
-                }
-            }
-            break;
-        }
-
-        if (bestMonitorCandidate)
-            return bestMonitorCandidate.id;
-
-        return null;
-    }
-
-    _getBestCandidate(id, monitor, activeRect) {
-        const windows = this._getAllWindows(monitor);
-        const { x, y } = activeRect;
-        let bestCandidate = null;
-        switch (id) {
-        case SCHEMA_SWAP_UP:
-            windows.forEach(w => {
-                const rect = w.get_frame_rect();
-                if (rect.y < y) {
-                    if (!bestCandidate) {
-                        bestCandidate = w;
-                    } else {
-                        const bestRect = bestCandidate.get_frame_rect();
-                        if (rect.x === bestRect.x && rect.y > bestRect.y)
-                            bestCandidate = w;
-                        else if (
-                            rect.x !== bestRect.x &&
-                Math.abs(activeRect.x - rect.x) < Math.abs(activeRect.x - bestRect.x)
-                        )
-                            bestCandidate = w;
-                    }
-                }
-            });
-            break;
-        case SCHEMA_SWAP_DOWN:
-            windows.forEach(w => {
-                const rect = w.get_frame_rect();
-                if (rect.y > y) {
-                    if (!bestCandidate) {
-                        bestCandidate = w;
-                    } else {
-                        const bestRect = bestCandidate.get_frame_rect();
-                        if (rect.x === bestRect.x && rect.y < bestRect.y)
-                            bestCandidate = w;
-                        else if (
-                            rect.x !== bestRect.x &&
-                Math.abs(activeRect.x - rect.x) < Math.abs(activeRect.x - bestRect.x)
-                        )
-                            bestCandidate = w;
-                    }
-                }
-            });
-            break;
-        case SCHEMA_SWAP_RIGHT:
-            windows.forEach(w => {
-                const rect = w.get_frame_rect();
-                if (rect.x > x) {
-                    if (!bestCandidate) {
-                        bestCandidate = w;
-                    } else {
-                        const bestRect = bestCandidate.get_frame_rect();
-                        if (rect.y === bestRect.y && rect.x < bestRect.x)
-                            bestCandidate = w;
-                        else if (
-                            rect.y !== bestRect.y &&
-                Math.abs(activeRect.y - rect.y) < Math.abs(activeRect.y - bestRect.y)
-                        )
-                            bestCandidate = w;
-                    }
-                }
-            });
-            break;
-        case SCHEMA_SWAP_LEFT:
-            windows.forEach(w => {
-                const rect = w.get_frame_rect();
-                if (rect.x < x) {
-                    if (!bestCandidate) {
-                        bestCandidate = w;
-                    } else {
-                        const bestRect = bestCandidate.get_frame_rect();
-                        if (rect.y === bestRect.y && rect.x > bestRect.x)
-                            bestCandidate = w;
-                        else if (
-                            rect.y !== bestRect.y &&
-                Math.abs(activeRect.y - rect.y) < Math.abs(activeRect.y - bestRect.y)
-                        )
-                            bestCandidate = w;
-                    }
-                }
-            });
-            break;
-        }
-
-        if (!bestCandidate) {
-            const newMonitor = this._getMonitorByDirection(id, monitor);
-            if (newMonitor !== null) {
-                return this._getBestCandidate(
-                    id,
-                    newMonitor,
-                    this._activeWindow.get_display().get_monitor_geometry(monitor)
-                );
-            }
-        }
-
-        return bestCandidate;
     }
 
     _getAllWindows(monitor) {
-        const workspace = this._workspaceManager.get_active_workspace();
+        const workspace = global.workspace_manager.get_active_workspace();
         const windows = workspace.list_windows();
         return windows.filter(w => w.get_monitor() === monitor && w.is_hidden() === false);
     }
 
     _getActiveWindow() {
-        const workspace = this._workspaceManager.get_active_workspace();
+        const workspace = global.workspace_manager.get_active_workspace();
         const windows = workspace.list_windows();
 
         let focusedWindow = null;
-        let focusedWindowRect = null;
 
         for (let window of windows) {
             if (window.has_focus()) {
                 focusedWindow = window;
-                focusedWindowRect = window.get_frame_rect();
                 break;
             }
         }
 
-        return { activeWindow: focusedWindow, activeRect: focusedWindowRect };
+        return focusedWindow;
     }
 
     _bindShortcut() {
