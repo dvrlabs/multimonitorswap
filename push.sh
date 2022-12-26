eb9e93fd0eee5176817831688900bc7298e778b2
DVR <dvrlabs.tv@gmail.com>
2022-12-26 01:34:09 -0500

    fixed push script

--- push.sh
+++ push.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 gnome_ext_dir=$HOME/.local/share/gnome-shell/extensions/multimonitorswap@dvrlabs.tv
-working_dir=`pwd`/*
+working_dir=`pwd`
 
-echo "Pushing $working_dir to $gnome_ext_dir"
-cp -r $working_dir $gnome_ext_dir 
+echo "PUSHING: \n" 
+echo $working_dir/* | tr " " "\n"
+echo "\n-->"
+echo ""
+echo $gnome_ext_dir
+echo "\n"
 
+cp -r $working_dir/*  $gnome_ext_dir
